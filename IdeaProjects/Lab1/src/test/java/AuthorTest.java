393dc87979285198d201e687ff5f7bcf21c79be2
cristinapan <pan.yuz@northeastern.edu>
2024-01-18 20:49:30 -0800

    Add Lab1

---
+++ IdeaProjects/Lab1/src/test/java/AuthorTest.java
@@ -0,0 +1,23 @@
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class AuthorTest {
+
+    @BeforeEach
+    void setUp() {
+    }
+
+    @Test
+    void getName() {
+    }
+
+    @Test
+    void getEmail() {
+    }
+
+    @Test
+    void getAddress() {
+    }
+}
